ec9ccbd3c71a985f16cdbe9f2184374c25eb16f6
Kijana Woodard <github@wyldeye.com>
2013-10-16 13:04:51 -0500

    initial commit

---
+++ src/LowercaseHypens.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Hotspots;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+
+namespace ResharperPlugins
+{
+	//https://github.com/markcapaldi/ReSharperMacros/blob/master/ReSharperMacros/TestFixtureVariableExpansionMacro.cs
+	//https://github.com/joaroyen/ReSharperExtensions
+
+	[Macro("CustomMacros.LowercaseHypens",
+	  ShortDescription = "Value of {#0:another variable} to lowercase and spaces to hyphens",
+	  LongDescription = "Take the value of {#0:another variable}, lowercase it, and replace spaces with hyphens")]
+    public class LowercaseHypens : IMacro
+    {
+	    public string GetPlaceholder(IDocument document)
+	    {
+		    return "a";
+	    }
+
+	    public bool HandleExpansion(IHotspotContext context, IList<string> arguments)
+	    {
+		    return false;
+	    }
+
+	    public HotspotItems GetLookupItems(IHotspotContext context, IList<string> arguments)
+	    {
+		    return null;
+	    }
+
+	    public string EvaluateQuickResult(IHotspotContext context, IList<string> arguments)
+	    {
+			if (arguments.Count != 1 || arguments[0] == null) return null;
+
+		    return arguments[0].ToLower().Replace(' ', '-');
+	    }
+
+		public ParameterInfo[] Parameters { get { return new[] { new ParameterInfo(ParameterType.VariableReference) }; } }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResharperPlugins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ResharperPlugins")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef15e52b-9a55-4aa6-8960-3a6ecf43b233")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
